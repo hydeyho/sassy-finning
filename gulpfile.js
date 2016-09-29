ce235d309cb6e27e5a36e8ca330c00278316619d
Matthew Hyde (UK) <mhyde@finning.com>
2016-09-29 10:34:09 +0100

    Added simple task to build sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   // place code for your default task here
+  return gulp.src('./src/sassy-finning.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./dist'));
 });
